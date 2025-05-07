88efb98ba06a63b837c22b4083ccb81404058b75
alh303 <alh303@txstate.edu>
2025-05-07 10:03:08 -0500

    Update DatabaseManager.java

--- app/src/main/java/org/example/DatabaseManager.java
+++ app/src/main/java/org/example/DatabaseManager.java
@@ -7,7 +7,7 @@ import org.bson.Document;
 import com.mongodb.client.*;
 
 public class DatabaseManager {
-    private static final String uri = "YOUR_MONGODB_CONNECTION_STRING";
+    private static final String uri = "mongodb+srv://alexlharp:<db_password>@cluster0.nbvjwqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     private static MongoClient mongoClient = MongoClients.create(uri);
     private static MongoDatabase database = mongoClient.getDatabase("musicplayer");
     private static MongoCollection<Document> collection = database.getCollection("songs");
